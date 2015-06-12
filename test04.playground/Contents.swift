92881a39dfa87141e774558c0b629b58dc155146
mcanche <mcanche@gmail.com>
2015-06-12 18:18:07 -0500

    testo4
    playground - tutorial 04

---
+++ test04.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+// +-------------+
+// | video 4     |
+// +-------------+
+func add(number1:Int, number2:Int) -> Int{
+    return number1 + number2
+}
+
+func getName() ->(String, String) {
+    
+    return ("Name 1","Name 2");
+}
+
+
+
+add(1,4)
+
+getName();
